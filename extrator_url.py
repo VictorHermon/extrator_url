1679754808ff752013a695ebb773905d6ecb2a6c
VictorHermon <victorsilva.silva6@gmail.com>
2021-12-02 22:00:01 -0300

    Verificando metodo false

--- extrator_url.py
+++ extrator_url.py
@@ -1,13 +1,13 @@
 class ExtratorURL:
+
     def __init__(self, url):
         self.url = self.sanitiza_url(url)
         self.valida_url()
 
-    def sanitiza_url(self, url):
-        if type(url) == str:
+    @staticmethod
+    def sanitiza_url(url):
+        if url:
             return url.strip()
-        else:
-            return ""
 
     def valida_url(self):
         if not self.url:
@@ -24,6 +24,8 @@ class ExtratorURL:
         return url_parametros
 
     def get_valor_parametro(self, parametro_busca):
+        if not parametro_busca:
+            raise ValueError('O parametro em get_valor_parametro, não pode ser 0 ou vazio')
         indice_parametro = self.get_url_parametros().find(parametro_busca)
         indice_valor = indice_parametro + len(parametro_busca) + 1
         indice_e_comercial = self.get_url_parametros().find('&', indice_valor)
@@ -36,6 +38,7 @@ class ExtratorURL:
 
 url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
 extrator_url = ExtratorURL(url)
-valor_quantidade = extrator_url.get_valor_parametro("quantidade")
+valor_quantidade = extrator_url.get_valor_parametro('quantidade')
 print(valor_quantidade)
+
 
