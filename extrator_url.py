9c6bc182f7189c06b3754d2bafc97b3353fefb07
VictorHermon <victorsilva.silva6@gmail.com>
2021-12-04 18:28:15 -0300

    Adicionado o conversor de moedas e validações usando Regex

--- extrator_url.py
+++ extrator_url.py
@@ -1,17 +1,27 @@
+import re
+
+
 class ExtratorURL:
 
     def __init__(self, url):
-        self.url = self.sanitiza_url(url)
+        self.url = self.sanetiza_url(url)
         self.valida_url()
 
     @staticmethod
-    def sanitiza_url(url):
+    def sanetiza_url(url):
         if url:
             return url.strip()
 
     def valida_url(self):
         if not self.url:
             raise ValueError("A URL está vazia")
+        # () para pegar o valor extato, [] para pegar as opções
+        padrao_url = re.compile('(http(s)?://)?(www.)?bytebank.com(.br)?/cambio')
+        # string para busca um padrao dentro de uma string
+        # match para verificar se a string combina com determinado padrao
+        match = padrao_url.match(url)
+        if not match:
+            raise ValueError('A URL não é valida')
 
     def get_url_base(self):
         indice_interrogacao = self.url.find('?')
@@ -35,10 +45,40 @@ class ExtratorURL:
             valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
         return valor
 
+    def __len__(self):
+        return len(self.url)
 
-url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+    def __str__(self):
+        return f'URL: {self.url} \nBase: {self.get_url_base()} \nParâmetros: {self.get_url_parametros()}'
+
+    def __eq__(self, other):
+        return self.url == other.url
+
+
+moeda_origem = input('Digite a moeda de origem (dolar ou real): ')
+moeda_destino = input('Digite a moeda de destino (dolar ou real): ')
+quantidade = input('Digite a quantidade de moedas: ')
+
+url = f'bytebank.com/cambio?quantidade={quantidade}&moedaOrigem={moeda_origem}&moedaDestino={moeda_destino}'
 extrator_url = ExtratorURL(url)
-valor_quantidade = extrator_url.get_valor_parametro('quantidade')
-print(valor_quantidade)
+valor_dolar = 5.50  # U$1 dolar = R$5.50 reais
 
+# extrator_url2 = ExtratorURL(url)
+# print(f'O tamanho da URL é igual á: {len(extrator_url)}')
+# print(extrator_url)
+# print(f'Os objetos são iguais: {extrator_url == extrator_url2}')
 
+moeda_origem = extrator_url.get_valor_parametro('moedaOrigem')
+moeda_destino = extrator_url.get_valor_parametro('moedaDestino')
+quantidade = extrator_url.get_valor_parametro('quantidade')
+
+if moeda_origem == 'real' and moeda_destino == 'dolar':
+    conversao = float(quantidade) / valor_dolar
+    print(f'Cotação do dolar: U${valor_dolar}')
+    print(f'Valor da conversão de {moeda_origem} para {moeda_destino} é: {conversao:.2f}')
+elif moeda_origem == 'dolar' and moeda_destino == 'real':
+    conversao = float(quantidade) * valor_dolar
+    print(f'Cotação do dolar: U${valor_dolar}')
+    print(f'Valor da conversão de {moeda_origem} para {moeda_destino} é: {conversao:.2f}')
+else:
+    print(f'O cambio de {moeda_origem} para {moeda_destino} não está disponivel')
